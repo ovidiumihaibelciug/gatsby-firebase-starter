ce7ed5f6e02d58118153104b6c4a3133a59cc0de
ovidiumihaibelciug <ovidiumihaibelciug@gmail.com>
2019-07-29 00:30:25 +0300

    style setup changes

---
+++ config.js
@@ -0,0 +1,40 @@
+'use strict';
+
+module.exports = {
+  url: 'http://mywebcomponents.com',
+  title: 'MyWebComponents',
+  subtitle:
+    'Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu.',
+  copyright: '© All rights reserved.',
+  disqusShortname: '',
+  postsPerPage: 4,
+  googleAnalyticsId: 'UA-135128268-2',
+  menu: [
+    {
+      label: 'Articles',
+      path: '/',
+    },
+    {
+      label: 'About me',
+      path: '/pages/about',
+    },
+    {
+      label: 'Contact me',
+      path: '/pages/contacts',
+    },
+  ],
+  author: {
+    name: 'John Doe',
+    photo: '/photo.jpg',
+    bio:
+      'Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu.',
+    contacts: {
+      email: '#',
+      telegram: '#',
+      twitter: '#',
+      github: '#',
+      rss: '#',
+      vkontakte: '#',
+    },
+  },
+};
