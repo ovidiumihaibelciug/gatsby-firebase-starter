6a8c20874552e5b5f96e1c3d3937a9a9f2914831
ovidiumihaibelciug <ovidiumihaibelciug@gmail.com>
2019-08-15 20:27:54 +0300

    Deactivate provider

--- src/components/scenes/Settings/molecules/LoginManagement/atoms/SocialLoginToggle.jsx
+++ src/components/scenes/Settings/molecules/LoginManagement/atoms/SocialLoginToggle.jsx
@@ -8,7 +8,18 @@ const SocialLoginToggle = ({
   onLink,
   onUnlink,
 }) =>
-  !isEnabled && (
+  isEnabled ? (
+    <button
+      type="button"
+      className={classNames(
+        'provider',
+        `provider--${signInMethod.name}`,
+      )}
+      onClick={() => onUnlink(signInMethod.id)}
+    >
+      <span>Deactivate</span> {<signInMethod.Icon />}
+    </button>
+  ) : (
     <button
       type="button"
       className={classNames(
