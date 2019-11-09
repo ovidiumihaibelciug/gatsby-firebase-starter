93ba8af647932cf512ed621d1e596dc18d6bd0be
Ovidiu-Mihai Belciug <ovidiumihaibelciug@gmail.com>
2019-11-10 01:56:17 +0200

    config changed

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,6 +14,7 @@ module.exports = {
       options: { prefixes: [`/post/*`] },
     },
     `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
