860b91e593ddb843a3518e24d917fc333fa9b24a
Ovidiu-Mihai Belciug <ovidiumihaibelciug@gmail.com>
2019-11-10 21:09:54 +0200

    Loading component changes

--- src/components/atoms/Loading.jsx
+++ src/components/atoms/Loading.jsx
@@ -3,14 +3,14 @@ import React from 'react';
 const Loading = () => (
   <div className="spinner__container">
     <div className="spinner__container__inner">
-      <div class="spinner">
-        <div class="set-one">
-          <div class="circle"></div>
-          <div class="circle"></div>
+      <div className="spinner">
+        <div className="set-one">
+          <div className="circle"></div>
+          <div className="circle"></div>
         </div>
-        <div class="set-two">
-          <div class="circle"></div>
-          <div class="circle"></div>
+        <div className="set-two">
+          <div className="circle"></div>
+          <div className="circle"></div>
         </div>
       </div>
     </div>
