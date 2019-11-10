2f9092647703545498a12c4a9082e56633aee5d9
Ovidiu-Mihai Belciug <ovidiumihaibelciug@gmail.com>
2019-11-10 15:56:46 +0200

    Deleted credentials

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,11 +1,11 @@
 'use strict';
 
 module.exports = {
-  apiKey: 'AIzaSyDbWIS7NPu5bZ7dOvMjleTYnYasTdm8qSA',
-  authDomain: 'mywebcomponentss.firebaseapp.com',
-  databaseURL: 'https://mywebcomponentss.firebaseio.com',
-  projectId: 'mywebcomponentss',
-  storageBucket: 'mywebcomponentss.appspot.com',
-  messagingSenderId: '45231235288',
-  appId: '1:45231235288:web:d6f40793a0d70703',
+  apiKey: '#',
+  authDomain: '#',
+  databaseURL: '#',
+  projectId: '#',
+  storageBucket: '#',
+  messagingSenderId: '#',
+  appId: '#',
 };
